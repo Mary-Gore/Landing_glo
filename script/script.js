27b3bb426b88e80bf78f5fa8c279b5b19b86880b
Maria Gorbunova <DM101Mary@gmail.com>
2021-09-02 00:04:43 +0400

    add files

---
+++ script/script.js
@@ -0,0 +1,63 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    //Таймер
+    const countTimer = deadline => {
+
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinuts = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        const getTimeRemaining = () => {
+            const dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+            return { timeRemaining, hours, minutes, seconds };
+        };
+
+        const updateClock = (id, callback) => {
+            const timer = getTimeRemaining();
+
+            if (timer.timeRemaining > 0) {
+
+                if (String(timer.hours).length === 1) {
+                    timerHours.textContent = '0' + timer.hours;
+                } else {
+                    timerHours.textContent = timer.hours;
+                }
+
+                if (String(timer.minutes).length === 1) {
+
+                    timerMinuts.textContent = '0' + timer.minutes;
+                } else {
+                    timerMinuts.textContent = timer.minutes;
+                }
+
+                if (String(timer.seconds).length === 1) {
+                    timerSeconds.textContent = '0' + timer.seconds;
+                } else {
+                    timerSeconds.textContent = timer.seconds;
+                }
+
+
+            } else {
+                callback(id);
+                timerHours.textContent = '00';
+                timerMinuts.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+        };
+
+        const IntervalId = setInterval(updateClock, 1000);
+
+        updateClock(IntervalId, id => {
+            clearInterval(id);
+        });
+    };
+
+
+    countTimer('2 september  2021');
+
+});
