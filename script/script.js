48d0d4d48b0d2ecb2183f55477d150d3cdd1e1ea
Maria Gorbunova <DM101Mary@gmail.com>
2021-09-19 00:56:56 +0400

    update less 28

--- script/script.js
+++ script/script.js
@@ -536,40 +536,41 @@ window.addEventListener('DOMContentLoaded', () => {
                 body[key] = val;
             });
 
-            const postData = (body, outputData, errorData) => {
+            const postData = body => {
 
-                const request = new XMLHttpRequest();
-                request.addEventListener('readystatechange', () => {
+                const promise = new Promise((resolve, reject) => {
+                    const request = new XMLHttpRequest();
+                    request.addEventListener('readystatechange', () => {
 
-                    if (request.readyState !== 4) {
-                        return;
-                    }
-                    if (request.status === 200) {
-                        outputData();
-                    } else {
-                        errorData(request.status);
-                    }
+                        if (request.readyState !== 4) {
+                            return;
+                        }
+                        if (request.status === 200) {
+                            resolve();
+                        } else {
+                            reject(request.status);
+                        }
+                    });
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type', 'application/json');
+                    request.send(JSON.stringify(body));
                 });
-                request.open('POST', 'server.php');
-                request.setRequestHeader('Content-Type', 'application/json');
 
-
-                request.send(JSON.stringify(body));
+                return promise;
             };
 
-            postData(body, () => {
-                statusMessage.textContent = successMessage;
-            }, error => {
-                statusMessage.textContent = errorMessage;
-                console.error(error);
-            });
+            postData(body)
+                .then(() => {
+                    statusMessage.textContent = successMessage;
+                })
+                .catch(error => {
+                    statusMessage.textContent = errorMessage;
+                    console.error(error);
+                });
         });
-
-
     };
 
     sendForm('form1');
     sendForm('form3');
     sendForm('form2');
-
 });
